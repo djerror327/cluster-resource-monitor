c3ed0fd817e4778634f721648bf7cb4edfcb55b3
djerror327 <djerror327@gmail.com>
2021-02-16 13:29:57 +0530

    add get method

--- src/main/java/com/dinusha/soft/controller/ResourceController.java
+++ src/main/java/com/dinusha/soft/controller/ResourceController.java
@@ -1,13 +1,35 @@
 package com.dinusha.soft.controller;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController("/")
 public class ResourceController {
+    private static final HashMap<String, String> payload = new HashMap<>();
+
     @PostMapping("/v1/cpu")
     public void persistData(@RequestBody String cpu) {
         System.out.println(cpu);
+        JSONParser jsonParser = new JSONParser();
+        try {
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(cpu);
+            payload.put((String) jsonObject.get("instance"), (String) jsonObject.get("cpu"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @GetMapping("/v1/cpu")
+    public Map<String, String> persistData() {
+        return payload;
     }
 }
