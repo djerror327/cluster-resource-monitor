b389318e4bbe94d879f59c15aa88a0216ae465c4
djerror327 <djerror327@gmail.com>
2021-02-17 10:24:01 +0530

    optimized code in backend

--- src/main/java/com/dinusha/soft/AppStart.java
+++ src/main/java/com/dinusha/soft/AppStart.java
@@ -1,12 +1,15 @@
 package com.dinusha.soft;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppStart {
+    private static final Logger logger = Logger.getLogger(AppStart.class);
 
     public static void main(String[] args) {
+        logger.info("Stating Cluster Resource Monitoring APP...");
         SpringApplication.run(AppStart.class, args);
     }
 }
--- src/main/java/com/dinusha/soft/controller/ResourceController.java
+++ src/main/java/com/dinusha/soft/controller/ResourceController.java
@@ -3,6 +3,7 @@ package com.dinusha.soft.controller;
 import com.dinusha.soft.utill.JsonUtil;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,10 +22,12 @@ public class ResourceController {
     private static final Logger logger = Logger.getLogger(ResourceController.class);
     private static HashMap<String, String> payload = new HashMap<>();
 
+    @Value("${delete.unused.tiles}")
+    private String deleteTiles;
+
     @PostMapping("/v1/cpu")
     public void persistData(@RequestBody String cpu) {
-//        System.out.println(cpu);
-        logger.debug(cpu);
+        logger.debug("Post call : /v1/cpu : " + cpu);
         JsonUtil.JSON_OBJECT.apply(cpu);
         JSONObject jsonObject = JsonUtil.JSON_OBJECT.apply(cpu);
         payload.put((String) jsonObject.get("instance"), (String) jsonObject.get("cpu"));
@@ -33,12 +36,17 @@ public class ResourceController {
 
     @GetMapping("/v1/cpu")
     public Map<String, String> persistData() {
+        logger.debug("Get call : /v1/cpu : " + payload);
         return payload;
     }
 
     @Scheduled(cron = "${cron.tile.refresh}")
     public void cleanTile() {
-//        System.out.println("deleting tiles");
-        payload = new HashMap<>();
+        if (deleteTiles.equals("true")) {
+            logger.debug("Deleting unused tiles");
+            payload = new HashMap<>();
+        } else {
+            logger.debug("Deleting unused tiles : off");
+        }
     }
 }
